a0165c0500b00224227d610646569e5604ab90b9
Tan Kuan Hoong <tkuanhoong@gmail.com>
2025-04-01 17:54:10 +0800

    remove: unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/nav-bar";
 import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "sonner";
