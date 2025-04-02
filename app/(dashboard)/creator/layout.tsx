3eb804a43d04fd82c390b043536cf6cab9323277
Tan Kuan Hoong <tkuanhoong@gmail.com>
2025-04-02 22:38:10 +0800

    new: dashboard layout

--- app/(dashboard)/creator/layout.tsx
+++ app/(dashboard)/creator/layout.tsx
@@ -1,7 +1,19 @@
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
+import DashboardHeader from "@/components/dashboard/dashboard-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
 export default function CreatorDashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <section>{children}</section>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <DashboardHeader />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
