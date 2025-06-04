f245bf66a400282380cafe00d90f82afbbc61c51
Tan Kuan Hoong <tkuanhoong@gmail.com>
2025-06-04 14:23:41 +0800

    update: prisma schema

---
+++ prisma/migrations/20250603141711_add_completed_progress_column_to_purchase_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `purchase` ADD COLUMN `completedProgressAt` DATETIME(3) NULL;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,46 @@
-import { PrismaClient } from '.prisma/client'
+import { PrismaClient, UserRole } from '.prisma/client'
+import { hashPassword } from '@/lib/hash';
 const prisma = new PrismaClient()
 
 async function main() {
     try {
+        const hashedPassword = await hashPassword("123456");
+        await prisma.user.createMany({
+            data: [
+                {
+                    name: "Student",
+                    identificationNo: '011111015541',
+                    email: "student@gmail.com",
+                    emailVerified: new Date(),
+                    password: hashedPassword,
+                    role: UserRole.USER
+                },
+                // {
+                //     name: "TAN KUAN HOONG",
+                //     identificationNo: '011201012943',
+                //     email: "tkuanhoong@gmail.com",
+                //     emailVerified: new Date(),
+                //     password: hashedPassword,
+                //     role: UserRole.USER
+                // },
+                {
+                    name: "Creator",
+                    identificationNo: '011201011011',
+                    email: "creator@gmail.com",
+                    emailVerified: new Date(),
+                    password: hashedPassword,
+                    role: UserRole.USER
+                },
+                {
+                    name: "Admin",
+                    identificationNo: '011124015582',
+                    email: "admin@gmail.com",
+                    emailVerified: new Date(),
+                    password: hashedPassword,
+                    role: UserRole.ADMIN
+                },
+            ]
+        });
         await prisma.category.createMany({
             data: [
                 { name: "Computer Science" },
