edb219859c5bf62247b728278e6548fa6efd9a82
Tan Kuan Hoong <tkuanhoong@gmail.com>
2025-06-17 18:18:04 +0800

    update: prisma db

---
+++ prisma/migrations/20250617101735_remove_is_free_field_from_section/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isFree` on the `section` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `section` DROP COLUMN `isFree`;
